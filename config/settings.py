e5ffd750470e0dd80f84815abfbec063964905a3
athif100 <115343752+athif100@users.noreply.github.com>
2025-02-15 08:24:43 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -132,4 +132,4 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 AUTH_USER_MODEL="core.MYUSERS"
 
-                            
+CSRF_TRUSTED_ORIGINS=["https://tourisim-production.up.railway.app"]
